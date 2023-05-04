4dbf9e3172d1cbc161be004d6742cba4b16c6a19
Carlos <carlosgdcj@hotmail.com>
2023-05-04 16:09:52 -0300

    Added focus management to todo creation

--- src/routes/NewTodo.svelte
+++ src/routes/NewTodo.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
+	import { createEventDispatcher, onMount } from 'svelte';
 
 	const dispatch = createEventDispatcher();
 
+	export let autofocus = false;
+
 	let name = '';
+	let nameEl: HTMLInputElement;
 
 	function addTodo() {
 		dispatch('addTodo', { name });
 		name = '';
+		nameEl.focus();
 	}
 
 	function onCancel() {
 		name = '';
+		nameEl.focus();
 	}
+
+	onMount(() => autofocus && nameEl.focus());
 </script>
 
 <!-- NewTodo -->
@@ -20,6 +27,13 @@
 	<h2 class="label-wrapper">
 		<label for="todo-0" class="label__lg"> What needs to be done? </label>
 	</h2>
-	<input type="text" id="todo-0" autocomplete="off" class="input input__lg" bind:value={name} />
+	<input
+		type="text"
+		id="todo-0"
+		autocomplete="off"
+		class="input input__lg"
+		bind:value={name}
+		bind:this={nameEl}
+	/>
 	<button type="submit" disabled={!name} class="btn btn__primary btn__lg"> Add </button>
 </form>
