1306760bbe2e0f0a2ffb7b379a38ab1e331c6033
Carlos <carlosgdcj@hotmail.com>
2023-05-16 21:24:02 -0300

    Added skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+	import '@skeletonlabs/skeleton/themes/theme-hamlindigo.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.css';
+
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+	<AppBar>
+		<h3 class="text-center">Todo App</h3>
+	</AppBar>
+	<slot />
+</AppShell>
